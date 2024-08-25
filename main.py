f9e0e993b603ace880e87e14799ae0da2847b25e
Georgy Davy <gadavy@star.intra.ispras.ru>
2024-08-25 22:24:06 +0300

    do the report feature + add total cost to purchases

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, jsonify, Blueprint, redirect, url_for, request
-
 from datetime import datetime, date
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField, DateField, BooleanField, FloatField
+from wtforms import StringField, SubmitField, DateField, BooleanField, FloatField, IntegerField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -70,6 +69,63 @@ class ProductForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+class Purchase(db.Model):
+    purchase_id = db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey(Buyer.buyer_id), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(Product.product_id), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    purchase_date = db.Column(db.Date)
+
+    def __init__(self, buyer_id, product_id, amount, purchase_date):
+        self.buyer_id = buyer_id
+        self.product_id = product_id
+        self.purchase_date = purchase_date
+        self.amount = amount
+
+
+class PurchaseForm(FlaskForm):
+    buyer = IntegerField('buyer', validators=[DataRequired()])
+    product = IntegerField('product', validators=[DataRequired()])
+    purchase_date = DateField('Purchase Date', validators=[DataRequired()])
+    amount = FloatField('Amount', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/purchases_page', methods=['GET', 'POST'])
+def purchases_page():
+    form = PurchaseForm()
+    if form.validate_on_submit():
+        purchase = Purchase(buyer_id=form.buyer.data, product_id=form.product.data,
+                            purchase_date=form.purchase_date.data, amount=form.amount.data)
+        db.session.add(purchase)
+        db.session.commit()
+        return redirect(url_for('purchases_page'))
+    purchases = Purchase.query.all()
+    return render_template('purchases_page.html', form=form, purchases=purchases)
+
+
+@app.route('/update_purchase/<int:purchase_id>', methods=['GET', 'POST'])
+def update_purchase(purchase_id):
+    purchase = Purchase.query.get_or_404(purchase_id)
+    form = PurchaseForm(obj=purchase)
+    if form.validate_on_submit():
+        purchase.product_id = form.product.data
+        purchase.buyer_id = form.buyer.data
+        purchase.amount = form.amount.data
+        purchase.purchase_date = form.purchase_date.data
+        db.session.commit()
+        return redirect(url_for('purchases_page'))
+    return render_template('update_purchase.html', form=form)
+
+
+@app.route('/delete_purchase/<int:purchase_id>', methods=['POST'])
+def delete_purchase(purchase_id):
+    purchase = Purchase.query.get_or_404(purchase_id)
+    db.session.delete(purchase)
+    db.session.commit()
+    return redirect(url_for('purchases_page'))
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -119,11 +175,6 @@ def update_buyer(buyer_id):
         db.session.commit()
         return redirect(url_for('buyers_page'))
     return render_template('update_buyer.html', form=form)
-
-
-'''
-    Products
-'''
 
 
 @app.route('/products_page', methods=['GET', 'POST'])
