8b08a35083fa2bb5957e6c9b72d104a633f43763
Georgy Davy <gadavy@star.intra.ispras.ru>
2024-08-29 01:32:02 +0300

    report works (still need to refactor files)

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from wtforms import StringField, SubmitField, DateField, BooleanField, FloatFiel
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-# csrf = CSRFProtect(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///task2.db"
 app.config['SECRET_KEY'] = 'key'
 db = SQLAlchemy(app)
@@ -209,6 +208,36 @@ def delete_product(product_id):
     db.session.delete(product)
     db.session.commit()
     return redirect(url_for('products_page'))
+
+
+def create_report(purchase_date):
+    purchases = Purchase.query.filter(Purchase.purchase_date == purchase_date).all()
+    buyers = list()
+    total_price = 0.0
+    for purchase in purchases:
+        buyer = Buyer.query.get_or_404(purchase.buyer_id)
+        buyers.append(f'{buyer.lastname}, {buyer.firstname}')
+
+        product = Product.query.get_or_404(purchase.product_id)
+        total_price += product.selling_price * purchase.amount
+
+    return buyers, total_price
+
+
+class DateForm(FlaskForm):
+    date = DateField('Date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/report_page', methods=['GET', 'POST'])
+def report_page():
+    form = DateForm()
+
+    if form.validate_on_submit():
+        purchase_date = form.date.data
+        buyers, total_price = create_report(purchase_date)
+        return render_template('report_page.html', form=form, buyers=buyers, total_price=total_price)
+    return render_template('report_page.html', form=form)
 
 
 def create_tables():
